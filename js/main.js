3cdc98dfede5fac343e49066a7cd0162f7dd6596
Tofazzal-13 <dev.tofazzal13@gmail.com>
2026-02-24 14:16:32 +0600

    interview and rejected toggle

--- js/main.js
+++ js/main.js
@@ -46,13 +46,26 @@ function toggleStyle(id) {
     selected.classList.remove("bg-gray-200", "text-[#64748B]")
     selected.classList.add("bg-[#3B82F6]", "text-white")
 
+    if(id === "interview_card_show"){
+        allCards.classList.add("hidden");
+        filteredSection.classList.remove("hidden")
+    }
+    else if(id === "all_card_show"){
+        allCards.classList.remove("hidden");
+        filteredSection.classList.add("hidden")
+    }
+    else if(id === "rejected_card_show"){
+        allCards.classList.add("hidden");
+        filteredSection.classList.remove("hidden")
+    }
+    
+
 }
 
 
 const mainContainer = document.querySelector("main");
 
 mainContainer.addEventListener("click", function (event) {
-    console.log(event.target.classList.contains("interview_btn"));
     
     if (event.target.classList.contains("interview_btn")) {
         const parentNode = event.target.parentNode.parentNode;
@@ -62,52 +75,116 @@ mainContainer.addEventListener("click", function (event) {
         const jobInformation = parentNode.querySelector(".job_information").innerText;
         const btnSatus = parentNode.querySelector(".btn_apply").innerText;
         const jobDes = parentNode.querySelector(".job_des").innerText;
-    
+
+        parentNode.querySelector(".btn_apply").classList.add("btn-outline","btn-success")
+        parentNode.querySelector(".btn_apply").classList.remove("btn-error")
+        parentNode.querySelector(".btn_apply").innerText = "INTERVIEW"
+        
         
         const cardInfo = {
             jobTitle,
             jobCircular,
             jobInformation,
             jobDes,
-            btnSatus
+            btnSatus:'INTERVIEW'
         }
 
         const jobTitleExit = interviewTotal.find(item => item.jobTitle == cardInfo.jobTitle)
-        parentNode.querySelector(".btn_apply").innerText = "INTERVIEW"
-        parentNode.querySelector(".btn_apply").classList.add("btn-outline","btn-success")
-        if (!jobTitleExit) {
+        if(!jobTitleExit) {
             interviewTotal.push(cardInfo)
         }
+
+        totalCount()
         renderInterview()
     }
+    if (event.target.classList.contains("rejected_btn")) {
+        const parentNode = event.target.parentNode.parentNode;
+
+        const jobTitle = parentNode.querySelector(".job_title").innerText;
+        const jobCircular = parentNode.querySelector(".job_circular").innerText;
+        const jobInformation = parentNode.querySelector(".job_information").innerText;
+        const btnSatus = parentNode.querySelector(".btn_apply").innerText;
+        const jobDes = parentNode.querySelector(".job_des").innerText;
+
+        parentNode.querySelector(".btn_apply").classList.add("btn-outline","btn-error")
+        parentNode.querySelector(".btn_apply").classList.remove("btn-success")
+        parentNode.querySelector(".btn_apply").innerText = "REJECTED"
+        
+        
+        const cardInfo = {
+            jobTitle,
+            jobCircular,
+            jobInformation,
+            jobDes,
+            btnSatus:'REJECTED'
+        }
+
+        const jobTitleExit = rejectedTotal.find(item => item.jobTitle == cardInfo.jobTitle)
+        if(!jobTitleExit) {
+            rejectedTotal.push(cardInfo)
+        }
+
+        totalCount()
+        renderRejected()
+    }
+    
 
 })
 
 // render interview section
-const filterSection = document.getElementById("filtered_section")
+const filterSection = document.getElementById("filteredSection")
 
 function renderInterview() {
     filterSection.innerHTML = '';
     for (let interview of interviewTotal) {
-        console.log(interview);
 
         let div = document.createElement("div")
         div.classList = "bg-white p-5 space-y-6"
         div.innerHTML = `
             <div class="flex justify-between">
                 <div class="space-y-2">
-                    <h3 class="text-[#002C5C] text-xl font-bold job_title">Mobile First Corp</h3>
-                    <p class="text-[#64748B] job_circular">React Native Developer</p>
+                    <h3 class="text-[#002C5C] text-xl font-bold job_title">${interview.jobTitle}</h3>
+                    <p class="text-[#64748B] job_circular">${interview.jobCircular}</p>
                 </div>
                 <div class="btn rounded-full w-8 h-8 ">
                     <i class="fa-solid fa-trash-can"></i>
                 </div>
             </div>
-            <p class="text-[#64748B] text-sm job_information">Remote • Full-time • $130,000 - $175,000</p>
+            <p class="text-[#64748B] text-sm job_information">${interview.jobInformation}</p>
             <div class="space-y-2">
-                <div class="btn btn_apply">NOT APPLIED</div>
-                <p class="text-[#323B49] job_des">Build cross-platform mobile applications using React Native. Work on products used by
-                    millions of users worldwide.</p>
+                <div class="btn btn_apply btn-outline btn-success">${interview.btnSatus}</div>
+                <p class="text-[#323B49] job_des">${interview.jobDes}</p>
+            </div>
+            <div>
+                <div class="btn btn-outline btn-success">INTERVIEW</div>
+                <div class="btn btn-outline btn-error">REJECTED</div>
+            </div>       
+        
+        
+        `
+        filterSection.appendChild(div)
+    }
+}
+function renderRejected() {
+    filterSection.innerHTML = '';
+    for (let rejected of rejectedTotal) {
+
+        let div = document.createElement("div")
+        div.classList = "bg-white p-5 space-y-6"
+        div.innerHTML = `
+            <div class="flex justify-between">
+                <div class="space-y-2">
+                    <h3 class="text-[#002C5C] text-xl font-bold job_title">${rejected.jobTitle}</h3>
+                    <p class="text-[#64748B] job_circular">${rejected.jobCircular}</p>
+                </div>
+                <div class="btn rounded-full w-8 h-8 ">
+                    <i class="fa-solid fa-trash-can"></i>
+                </div>
+            </div>
+            <p class="text-[#64748B] text-sm job_information">${rejected.jobInformation}</p>
+            <div class="space-y-2">
+                <div class="btn btn_apply btn-outline btn-error">${rejected.btnSatus}</div>
+                <p class="text-[#323B49] job_des">${rejected.jobDes}</p>
             </div>
             <div>
                 <div class="btn btn-outline btn-success">INTERVIEW</div>
